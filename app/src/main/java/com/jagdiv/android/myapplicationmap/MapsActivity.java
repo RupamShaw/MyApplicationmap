adedd1af17c46fef6c6f79a55078420e1c2a4a5a
Dell <rupam.shaw@gmail.com>
2016-03-18 13:21:13 +1100

    changes on comment of class 2nd time

--- app/src/main/java/com/jagdiv/android/myapplicationmap/MapsActivity.java
+++ app/src/main/java/com/jagdiv/android/myapplicationmap/MapsActivity.java
@@ -34,12 +34,15 @@ import java.util.Locale;
 
 import javax.net.ssl.HttpsURLConnection;
 
+/**
+ * Can be used by extending activity or extend fragment activity and implements onMapReadyCallback
+ */
 public class MapsActivity extends Activity {// FragmentActivity // implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
     ArrayList<InfoPoint> resultspoint = new ArrayList<InfoPoint>();
     double lng = 0.0;
     double lat = 0.0;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +57,12 @@ public class MapsActivity extends Activity {// FragmentActivity // implements On
         mMap.setIndoorEnabled(true);
         mMap.setBuildingsEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
-       // mapFragment.getMapAsync(this);
+        // mapFragment.getMapAsync(this);
         LatLng addresslatlng = new LatLng(25, 74);
         mMap.addMarker(new MarkerOptions().position(addresslatlng).title("Marker in guess"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(addresslatlng));
-       // CameraUpdate update = CameraUpdateFactory.newLatLngZoom(addresslatlng, 15);
-       // mMap.moveCamera(update);
+        // CameraUpdate update = CameraUpdateFactory.newLatLngZoom(addresslatlng, 15);
+        // mMap.moveCamera(update);
 
         System.out.println("22222222222222nd type");
         //2nd we can get by url
@@ -67,90 +70,11 @@ public class MapsActivity extends Activity {// FragmentActivity // implements On
         getLatLngfromJson(address);
     }
 
-    private class DataLongOperationAsynchTask extends AsyncTask<String, Void, String[]> {
-        ProgressDialog dialog = new ProgressDialog(MapsActivity.this);
-        String addr="";
-        private final GoogleMap mMap;
-
-// ---------------------------------------------------------
-
-        public DataLongOperationAsynchTask( GoogleMap gmap)
-        {
-
-            this.mMap = gmap;
-        }
-
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            dialog.setMessage("Please wait...");
-            dialog.setCanceledOnTouchOutside(false);
-            dialog.show();
-        }
-
-        @Override
-        protected String[] doInBackground(String... params) {
-            String response;
-            try {
-                // "http://maps.googleapis.com/maps/api/geocode/json?address={address}&sensor=false";
-                 addr = params[0].toString();
-                response = getRespByURL("http://maps.googleapis.com/maps/api/geocode/json?address=" + addr + "&sensor=false");
-                System.out.println("response" + response);
-                return new String[]{response};
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("here in getting response error");
-                return new String[]{"error"};
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String... result) {
-            // GoogleMap mMap=GoogleMap;
-            /*try {
-                JSONObject jsonObject = new JSONObject(result[0]);
-
-                 lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
-                        .getJSONObject("geometry").getJSONObject("location")
-                        .getDouble("lng");
-
-                 lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
-                        .getJSONObject("geometry").getJSONObject("location")
-                        .getDouble("lat");
-
-                Log.d("latitude", "" + lat);
-                Log.d("longitude", "" + lng);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }*/
-            //ArrayList<InfoPoint> resultspoint=new ArrayList<InfoPoint>();
-
-            resultspoint = parsePoints(result[0]);
-            getLatlngInfoPoint(resultspoint);
-            InfoPoint point = resultspoint.get(0);
-            LatLng addresslatlng = new LatLng(point.getDblLatitude(), point.getDblLongitude());
-            LatLng addresslatlng1 = new LatLng(28, 79   );
-            mMap.addMarker(new MarkerOptions().position(addresslatlng).title("Marker in" + addr+point.getDblLatitude()+","+point.getDblLongitude()));
-            mMap.addMarker(new MarkerOptions().position(addresslatlng1).title("Marker in" + addresslatlng1.describeContents()+addresslatlng1.latitude+","+addresslatlng1.longitude));
-
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(addresslatlng));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(addresslatlng1));
-
-
-          //  CameraUpdate update = CameraUpdateFactory.newLatLngZoom(addresslatlng, 1);
-            //mMap.moveCamera(update);
-            if (dialog.isShowing()) {
-                dialog.dismiss();
-            }
-        }
-    }
-
     private void getLatlngInfoPoint(ArrayList<InfoPoint> results) {
         InfoPoint point = new InfoPoint();
         point = results.get(0);
-       lng= point.getDblLongitude();
-        lat=point.getDblLatitude();
+        lng = point.getDblLongitude();
+        lat = point.getDblLatitude();
         System.out.println("point.getDblLongitude()" + point.getDblLongitude() + " point.getDblLatitude() " + point.getDblLatitude());
     }
 
@@ -228,46 +152,6 @@ public class MapsActivity extends Activity {// FragmentActivity // implements On
         return result;
     }
 
-    public class InfoPoint {
-        ArrayList<HashMap<String, Object>> addressFields = new ArrayList<HashMap<String, Object>>();
-        String strFormattedAddress = "";
-        double dblLatitude = 0;
-        double dblLongitude = 0;
-
-        public ArrayList<HashMap<String, Object>> getAddressFields() {
-            return addressFields;
-        }
-
-        public void setAddressFields(ArrayList<HashMap<String, Object>> addressFields) {
-            this.addressFields = addressFields;
-        }
-
-        public String getStrFormattedAddress() {
-            return strFormattedAddress;
-        }
-
-        public void setStrFormattedAddress(String strFormattedAddress) {
-            this.strFormattedAddress = strFormattedAddress;
-        }
-
-        public double getDblLatitude() {
-            return dblLatitude;
-        }
-
-        public void setDblLatitude(double dblLatitude) {
-            this.dblLatitude = dblLatitude;
-        }
-
-        public double getDblLongitude() {
-            return dblLongitude;
-        }
-
-        public void setDblLongitude(double dblLongitude) {
-            this.dblLongitude = dblLongitude;
-        }
-
-    }
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -277,12 +161,12 @@ public class MapsActivity extends Activity {// FragmentActivity // implements On
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-   // @Override
+    // @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         String address = "mumbai";
         getLatLngfromJson(address);
-     //this will not work because calls are only handled in async task we cant get lat long from async task to this main thread
+        //this will not work because calls are only handled in async task we cant get lat long from async task to this main thread
     /*    if (resultspoint != null && resultspoint.size() > 0) {
             InfoPoint point = resultspoint.get(0);
             System.out.println("point.getDblLongitude()" + point.getDblLongitude() + " point.getDblLatitude() " + point.getDblLatitude());
@@ -294,7 +178,7 @@ public class MapsActivity extends Activity {// FragmentActivity // implements On
             mMap.moveCamera(update);
         } else
             System.out.println("problem in getting address");*/
-       System.out.println("lat " + lat + " lng " + lng);
+        System.out.println("lat " + lat + " lng " + lng);
 
   /*      LatLng addresslatlng = new LatLng(lat, lng);
         mMap.addMarker(new MarkerOptions().position(addresslatlng).title("Marker in" + address+" "+lat+","+lng));
@@ -313,7 +197,7 @@ public class MapsActivity extends Activity {// FragmentActivity // implements On
     }
 
     private void getLatLngfromJson(String address) {
-       (new DataLongOperationAsynchTask(mMap)).execute(new String[]{address});
+        (new DataLongOperationAsynchTask(mMap)).execute(new String[]{address});
     }
 
     private void gotoLocation(double lat, double lon, float zoom) {
@@ -369,6 +253,124 @@ public class MapsActivity extends Activity {// FragmentActivity // implements On
             locationLabel.setText("Need to check location");*/
         // Location currentLocation=new Location()
         (new GetAddressTask(this)).execute(new String[]{"latlong"});
+    }
+
+    private class DataLongOperationAsynchTask extends AsyncTask<String, Void, String[]> {
+        private final GoogleMap mMap;
+        ProgressDialog dialog = new ProgressDialog(MapsActivity.this);
+        String addr = "";
+
+// ---------------------------------------------------------
+
+        public DataLongOperationAsynchTask(GoogleMap gmap) {
+
+            this.mMap = gmap;
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please wait...");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            String response;
+            try {
+                // "http://maps.googleapis.com/maps/api/geocode/json?address={address}&sensor=false";
+                addr = params[0].toString();
+                response = getRespByURL("http://maps.googleapis.com/maps/api/geocode/json?address=" + addr + "&sensor=false");
+                System.out.println("response" + response);
+                return new String[]{response};
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("here in getting response error");
+                return new String[]{"error"};
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String... result) {
+            // GoogleMap mMap=GoogleMap;
+            /*try {
+                JSONObject jsonObject = new JSONObject(result[0]);
+
+                 lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                        .getJSONObject("geometry").getJSONObject("location")
+                        .getDouble("lng");
+
+                 lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                        .getJSONObject("geometry").getJSONObject("location")
+                        .getDouble("lat");
+
+                Log.d("latitude", "" + lat);
+                Log.d("longitude", "" + lng);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }*/
+            //ArrayList<InfoPoint> resultspoint=new ArrayList<InfoPoint>();
+
+            resultspoint = parsePoints(result[0]);
+            getLatlngInfoPoint(resultspoint);
+            InfoPoint point = resultspoint.get(0);
+            LatLng addresslatlng = new LatLng(point.getDblLatitude(), point.getDblLongitude());
+            LatLng addresslatlng1 = new LatLng(28, 79);
+            mMap.addMarker(new MarkerOptions().position(addresslatlng).title("Marker in" + addr + point.getDblLatitude() + "," + point.getDblLongitude()));
+            mMap.addMarker(new MarkerOptions().position(addresslatlng1).title("Marker in" + addresslatlng1.describeContents() + addresslatlng1.latitude + "," + addresslatlng1.longitude));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(addresslatlng));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(addresslatlng1));
+
+
+            //  CameraUpdate update = CameraUpdateFactory.newLatLngZoom(addresslatlng, 1);
+            //mMap.moveCamera(update);
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+        }
+    }
+
+    public class InfoPoint {
+        ArrayList<HashMap<String, Object>> addressFields = new ArrayList<HashMap<String, Object>>();
+        String strFormattedAddress = "";
+        double dblLatitude = 0;
+        double dblLongitude = 0;
+
+        public ArrayList<HashMap<String, Object>> getAddressFields() {
+            return addressFields;
+        }
+
+        public void setAddressFields(ArrayList<HashMap<String, Object>> addressFields) {
+            this.addressFields = addressFields;
+        }
+
+        public String getStrFormattedAddress() {
+            return strFormattedAddress;
+        }
+
+        public void setStrFormattedAddress(String strFormattedAddress) {
+            this.strFormattedAddress = strFormattedAddress;
+        }
+
+        public double getDblLatitude() {
+            return dblLatitude;
+        }
+
+        public void setDblLatitude(double dblLatitude) {
+            this.dblLatitude = dblLatitude;
+        }
+
+        public double getDblLongitude() {
+            return dblLongitude;
+        }
+
+        public void setDblLongitude(double dblLongitude) {
+            this.dblLongitude = dblLongitude;
+        }
+
     }
 
     /*
